e7862f535f0a8ae0a3ffa34442e84ba0e00d6a24
bkwrdbailey <baileybarton77@yahoo.com>
2024-12-13 10:53:11 -0600

    First Commit containing the initial console project setup, the csv file to extract data from, and the Person class with the needed fields/properties defined within.

---
+++ ETL-demo-12-12-2024_FollowUp/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETL_demo_12_12_2024_FollowUp.Models
+{
+    public class Person
+    {
+        private int Id { get; set; }
+        private string Name { get; set; } = string.Empty;
+        private int Age { get; set; }
+        private string City { get; set; } = string.Empty;
+    }
+}
